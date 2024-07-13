e52763c92b646684121d9486c2d8fbd936fc5cf1
Jenish Dongol <110826725+Jenis980@users.noreply.github.com>
2024-07-13 11:25:03 +0000

    create string and integer literal in array

--- Array/Variables.cs
+++ Array/Variables.cs
@@ -1,3 +1,9 @@
  // array is defined by variable tyoe with square bracket
   string[] cars;
 
+// to insert value in string array
+// values are inserted within curly braces separated by comma
+string[] cars = {"Toyota", "Ford", "BMW"};
+
+// to insert integer value
+int[] num = {1, 2, 6};
