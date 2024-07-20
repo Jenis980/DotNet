86601d65d2d73baf328ce4dc83c27fb14d640ff7
Jenish Dongol <110826725+Jenis980@users.noreply.github.com>
2024-07-20 15:19:27 +0000

    ok finally on c# One codespace

---
+++ AdditionOp/Addition.cs
@@ -0,0 +1,10 @@
+namespace AdditionOp
+{
+    public class AdditionOp
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ AdditionOp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace AdditionOp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            AdditionOp Obj1 = new AdditionOp();
+            int result = Obj1.Add(1,2);
+            Console.WriteLine($"Result is {result}");
+        }
+    }
+}
